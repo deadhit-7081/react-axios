48e952c2ebcf438169fe23543a7cc9fa671cefdc
Jai <jaikumarsingh1908@gmail.com>
2020-04-12 07:02:07 +0530

    useState Hook Counter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import PostList from './components/PostList';
+import HookCounter from './components/HookCounter';
 
 function App() {
   return (
     <div className="App">
-      <PostList/>
+      <HookCounter/>
     </div>
   );
 }
