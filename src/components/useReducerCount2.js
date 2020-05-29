df526a2b6d88acaa19fb4b5e18a953c90c059ad2
Jai <jaikumarsingh1908@gmail.com>
2020-05-29 11:38:53 +0530

    useReducer state as object

--- src/components/useReducerCount2.js
+++ src/components/useReducerCount2.js
@@ -2,15 +2,20 @@ import React , {useReducer} from 'react';
 
 const initialState = {
     firstCounter: 0,
+    secondCounter: 10
 }
 const reducer = (state,action) =>
 {
     switch(action.type)
     {
         case 'increment':
-            return {firstCounter :state.firstCounter + action.value}
+            return {...state, firstCounter :state.firstCounter + action.value}
         case 'decrement':
-            return {firstCounter :state.firstCounter - action.value}
+            return {...state,firstCounter :state.firstCounter - action.value}
+        case 'increment2':
+            return {...state,secondCounter :state.secondCounter + action.value}
+        case 'decrement2':
+            return {...state,secondCounter :state.secondCounter - action.value}
         case 'reset':
             return initialState
         default:
@@ -25,11 +30,16 @@ function CounterTwo()
     return(
         <div>
             <div>Count:{count.firstCounter}</div>
+            <div>SecCount : {count.secondCounter}</div>
             <button onClick = {() => dispatch({type:'increment',value : 1})}>Increment</button>
             <button onClick = {() => dispatch({type:'decrement',value : 1})}>Decrement</button>
             <button onClick = {() => dispatch({type :'reset'})}>Reset</button>
             <button onClick = {() => dispatch({type:'increment',value : 5})}>Increment5</button>
             <button onClick = {() => dispatch({type:'decrement',value : 5})}>Decrement5</button>
+            <div>
+            <button onClick = {() => dispatch({type:'increment2',value : 1})}>Increment2</button>
+            <button onClick = {() => dispatch({type:'decrement2',value : 1})}>Decrement2</button>
+            </div>
         </div>
     )
 }
