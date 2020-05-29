6e9e2cebee9d1c9933bb512c2aad0d0e64b90d0e
Jai <jaikumarsingh1908@gmail.com>
2020-05-29 11:25:07 +0530

    useReducer counter state and action as object

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import CounterOne from './components/useReducercount1';
+import CounterTwo from './components/useReducerCount2';
 
 
 function App() {
   return (
     <div className="App">
       <CounterOne/>
+      <CounterTwo/>
     </div>
   );
 }
