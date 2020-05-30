1ad764c39bb43fa34d7b5e72b4d5d0aca828cacf
Jai <jaikumarsingh1908@gmail.com>
2020-05-30 12:01:58 +0530

    Fetching and Loading useReducer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
 import DataFetchingOne from './components/DatafetchuseReducer';
+import DataFetchingTwo from './components/DataFetchingTwo';
 
 
 
 function App() {
   return (
     <div className="App">
-      <DataFetchingOne/>
+      <DataFetchingTwo/>
     </div>
   );
 }
